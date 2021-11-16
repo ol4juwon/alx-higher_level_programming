6ff438bc7331c11d0675cba63ea15beb23166c81
olajuwon lawal <olajuwonlawal2012@gmail.com>
2021-11-16 00:57:44 -0800

    added abs to last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = number * -1
+last = abs(number) % 10
 if(last > 5):
     print('Last digit of {} is {} and is greater than 5'.format(number, last))
 elif(last == 0):
