6642d759946d3b5bcd763bfcc9e2473ec86233b6
olajuwon lawal <olajuwonlawal2012@gmail.com>
2021-11-16 07:16:35 -0800

    dlf

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if(number <= 0):
-        number = number * -1
+    number = number * -1
     last_number = number % 10
     print('{}'.format(last_number))
     return last_number
