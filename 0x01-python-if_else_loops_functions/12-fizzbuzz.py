c3a2274e1ce339ba02681012fdf312e5ea8cee0e
olajuwon lawal <olajuwonlawal2012@gmail.com>
2021-11-16 08:02:54 -0800

    dfwew

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
     for i in range(1, 101):
         numer = i % 3
-        if(numer == 0):
+        if numer == 0:
             print('fizz', end=' ')
         else:
             print('{}'.format(i), end=' ')
