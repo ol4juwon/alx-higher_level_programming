e52ac69a23745c967e4086b71c23920a01e881fc
olajuwon lawal <olajuwonlawal2012@gmail.com>
2021-11-16 08:12:47 -0800

    lool

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
     if i % 2 != 0:
-        print("{:c}".format(i - 32), end='')
-    else:
-        print("{:c}".format(i), end='')
+        i = i-32
+    print("{:c}".format(i), end='')
