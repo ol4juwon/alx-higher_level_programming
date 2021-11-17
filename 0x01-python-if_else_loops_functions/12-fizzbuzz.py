e598d9277b727c0759b4273fae4e6d247823596f
olajuwon lawal <olajuwonlawal2012@gmail.com>
2021-11-17 14:02:35 -0800

    fix fizz buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,10 @@
 def fizzbuzz():
     for i in range(1, 101):
         numer = i % 3
+        buzz = i % 5
         if numer == 0:
             print('fizz', end=' ')
+        elif buzz == 0:
+            print('buzz', end=' ')
         else:
             print('{}'.format(i), end=' ')
