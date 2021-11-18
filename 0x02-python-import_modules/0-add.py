11028338992b718900288935ea0f4d721521c3cb
olajuwon lawal <olajuwonlawal2012@gmail.com>
2021-11-18 11:33:35 -0800

    add imports

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
->>> import add_0.py
-add_o.add(1+2)
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print('{0} + {1} = {2}'.format(a, b, add(a, b)))
