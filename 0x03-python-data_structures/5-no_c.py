d6e939e9ac09a95570f0ae1313a7ee29e2644212
olajuwon lawal <olajuwonlawal2012@gmail.com>
2021-11-21 13:21:32 -0800

    no cee

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+    if not my_string:
+        pass
+    else:
+        while i < len(my_string):
+            if my_string[i] not "c" or my_string[i] not "C":
+                new_string.append(my_string[i])
+    return new_string
