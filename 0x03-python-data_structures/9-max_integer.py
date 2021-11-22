d5bad3523a996efc5a6fc481919f5c854d4b3041
olajuwon lawal <olajuwonlawal2012@gmail.com>
2021-11-22 11:00:12 -0800

    MAx xAM

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,8 +4,8 @@ def max_integer(my_list=[]):
     if lent < 1:
         return "None"
     else:
-        max_length  = my_list[0]
-        i = 1;
+        max_length = my_list[0]
+        i = 1
         while i < lent:
             if my_list[i] > max_length:
                 max_length = my_list[i]
