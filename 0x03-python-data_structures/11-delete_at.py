28bfc8d92d68f5be9138ffac193fe8bf086b8ad3
olajuwon lawal <olajuwonlawal2012@gmail.com>
2021-11-22 12:29:20 -0800

    splice edit

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,6 +3,5 @@ def delete_at(my_list=[], idx=0):
     if len(my_list) < idx < 0:
         return my_list
     else:
-        index = idx + 1
-        new_list = my_list[:idx] + my_list[index:]
-        return new_list
+        del my_list[idx]
+        return (my_list)
