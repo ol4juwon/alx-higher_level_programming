883c5bb415ecffbbb8fa903143ab7b42b92e466c
olajuwon lawal <olajuwonlawal2012@gmail.com>
2021-11-25 11:26:28 -0800

    instance of str fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 def roman_to_int(s):
-    if not s or s == "None":
+    if s == "None":
         return 0
-    rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    val = 0
-    for i in range(len(s)):
-        if i > 0 and rom[s[i]] > rom[s[i-1]]:
-            val += rom[s[i]] - 2 * rom[s[i-1]]
-        else:
-            val += rom[s[i]]
-    return val
+    if isinstance(s, (str)):
+        rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        val = 0
+        for i in range(len(s)):
+            if i > 0 and rom[s[i]] > rom[s[i-1]]:
+                val += rom[s[i]] - 2 * rom[s[i-1]]
+            else:
+                val += rom[s[i]]
+        return val
+    else:
+        return 0
