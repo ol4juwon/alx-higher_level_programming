6ee8654d3aea000301db82255f41d7bb0555bd14
olajuwon lawal <olajuwonlawal2012@gmail.com>
2021-11-25 09:31:45 -0800

    fixed typo errors

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def multiply_by_2(a_dicitonary):
-    for i in a_dcitionary:
+def multiply_by_2(a_dictionary):
+    for i in a_dictionary:
         a_dictionary[i] = a_dictionary[i] * 2
     return a_dictionary
