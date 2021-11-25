64f86e9be60c7d9e23c72c58a7c82e6e79607470
olajuwon lawal <olajuwonlawal2012@gmail.com>
2021-11-25 09:18:26 -0800

    if a in b fix

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if a_dictionary.hasKey(key):
+    if key in a_dictionary:
         a_dictionary[key] = value
     else:
         a_dictionary.update({key: value)}
