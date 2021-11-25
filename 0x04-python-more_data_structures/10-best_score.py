7dce4ae52bd1af83c60a4e688f30cb5da42a9780
olajuwon lawal <olajuwonlawal2012@gmail.com>
2021-11-25 10:57:03 -0800

    pcs fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -8,5 +8,5 @@ def best_score(a_dict):
         for x in a_dict:
             if a_dict[x] > best_score:
                 best_score = a_dict[x]
-                best_key = x 
+                best_key = x
         return best_key
