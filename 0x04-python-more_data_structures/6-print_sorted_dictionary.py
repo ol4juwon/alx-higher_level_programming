d9eee39eef9e42036c9b2f350c03095d981c5d0b
olajuwon lawal <olajuwonlawal2012@gmail.com>
2021-11-25 09:06:27 -0800

    sorted

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    new_dict = {}
     for i in sorted(a_dictionary):
-        new_dict[i] = a_dictionary[i]
-    return new_dict
+        print("{0}: {1}".format(i, a_dictionary[i]), end="\n")
