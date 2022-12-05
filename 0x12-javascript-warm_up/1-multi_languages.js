b15ee9076d2326eb48f890c51d252b6317c69b83
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-05 12:32:13 -0800

    semistandard ish

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const cis= "C is fun";
-const pyt = "Python is cool";
-const js = "JavaScript is amazing";
+const cis = 'C is fun';
+const pyt = 'Python is coo';
+const js = 'JavaScript is amazing';
 console.log(cis);
 console.log(pyt);
 console.log(js);
