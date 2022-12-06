6c00ee40dae04fc48d6089c708cc5603bcaa9365
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-06 12:27:51 +0100

    add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const add = async (a, b) => {
-    const sum = a + b;
-console.log(sum);
+  const sum = a + b;
+  console.log(sum);
 };
 const a = parseInt(process.argv[2]);
 const b = parseInt(process.argv[3]);
