d1b967696ee1d877e1579db8d3f9d0038855d111
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-06 12:34:02 +0100

    s

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+// #!/usr/bin/node
+const c = parseInt(process.argv[2]);
+const factorial = ( num ) => {
+  let fact = 1;
+  while( num > 0){
+    fact *= num;
+    num--;
+}
+return fact;
+};
+console.log(factorial(c));
