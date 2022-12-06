fd2800aa0882981a261c73eb6f4856f9dadea98f
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-06 19:57:11 +0100

    sorted array

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,15 +2,13 @@
 const { dict } = require('./101-dict');
 const keys = Object.keys(dict);
 const values = Object.values(dict);
-
-console.log(keys, values);
 let matched;
 let index;
 const result = {};
 for (let i = 0; i < values.length; i++) {
   index = JSON.stringify(values[i]);
   result[index] = [];
-  matched = keys.filter((key) => values[index] === dict[key]);
+  matched = keys.filter((key) => values[i] === dict[key]);
   matched.forEach((item) => { result[index].push(item); });
 }
 console.log(result);
