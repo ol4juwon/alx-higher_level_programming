34a7e1c99fa1bbb1da7719c9a90012f034b57335
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-06 11:37:53 +0100

    for in fix

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const langs = ['C is fun','Python is cool','JavaScript is amazing'];
+const langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const land in langs) {
   console.log(`${langs[land]}`);
 }
