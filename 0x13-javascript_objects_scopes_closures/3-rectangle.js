97640cf1764619b2932c2f29cbfbdcefadb69058
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-06 16:15:39 +0100

    4-rectangle

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+// #!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let newX = '';
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.height; j++) {
+        newX += 'X';
+      }
+      console.log(newX);
+    }
+  }
+}
+module.exports = Rectangle;
