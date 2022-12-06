7c4e393d5c665b54ffa9bf87c4eaff7c669eb133
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-06 12:15:37 +0100

    semistandanr

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,14 @@
 #!/usr/bin/node
-let x = process.argv[2];
+const x = process.argv[2];
 if (isNaN(x)) {
   console.log('Missing size');
 }
-let i = 0, j = 0;
-let newx = '' ;
+let i = 0;
+let j = 0;
+let newx = '';
 for (; i < x; i++) {
-    for (; j < x; j++) {
-        newx += 'X';
-    }
-    console.log(newx);
+  for (; j < x; j++) {
+    newx += 'X';
+  }
+  console.log(newx);
 }
