189a69cd3dbb7bda776ecb5cfd8f9ac94c59a883
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-06 11:52:32 +0100

    multi c

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const x = process.argv[2];
-if (!isNaN(x)) {
-  while(x > 0) {
-    console.log('C is fun');
-    x--;
-  }
-} else {
+if (isNaN(x)) {
   console.log('Missing number of occurrences');
+  return;
+}
+while(x > 0) {
+  console.log('C is fun');
+  x--;
 }
