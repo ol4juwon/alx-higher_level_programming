cab51079257a3e1c0a12f1fd642ef61697fe5c2e
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-06 11:31:09 +0100

    isNan

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 const str = process.argv[2];
 if (!isNaN(parseInt(str))) {
-console.log(`My number: ${str}`);
+  console.log(`My number: ${str}`);
+} else {
+  console.log('Not a number');
 }
