897186a89610f82fc8fce9c0618b363bafbf961b
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-06 19:00:19 +0100

    fix for map

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,6 @@
+// #!/usr/bin/node
+const x = require('./100-data.js');
+const { list } = x;
+console.log(list);
+const newList = list.map((item, index) => { return item * index; });
+console.log(newList);
