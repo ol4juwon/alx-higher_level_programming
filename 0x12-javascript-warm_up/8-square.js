232871801cd651c345b35065ce7964a18af2647d
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-06 12:12:32 +0100

    lo

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+let x = process.argv[2];
+if (isNaN(x)) {
+  console.log('Missing size');
+}
+let i = 0, j = 0;
+let newx = '' ;
+for (; i < x; i++) {
+    for (; j < x; j++) {
+        newx += 'X';
+    }
+    console.log(newx);
+}
