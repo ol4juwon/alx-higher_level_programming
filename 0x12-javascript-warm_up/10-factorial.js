d46074f252e35790a94959dffda14b10cb75b049
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-06 12:50:13 +0100

    convert to funtion

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 const c = parseInt(process.argv[2]);
 function factorial (num) {
-  if(isNaN(num) || num === 0)
+  if (isNaN(num) || num === 0) {
     return 1;
+  }
   return (num * factorial(num - 1));
 }
 console.log(factorial(Number(c)));
