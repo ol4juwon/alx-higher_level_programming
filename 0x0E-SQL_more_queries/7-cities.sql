6c964a872c959d63e3839caf8b4ff9ebe98bfb31
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-11 17:53:46 +0100

    create force

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,9 @@
 -- create force table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 id INT AUTO_INCREMENT NOT NULL UNIQUE,
 state_id INT,
 name VARCHAR(256) NOT NULL
-FOREIGN_KEY (state_id) REFERENCES states(state_id)
+FOREIGN_KEY (state_id) REFERENCES states(id)
 );
