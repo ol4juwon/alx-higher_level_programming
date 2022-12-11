840a906a267650632560d87f4d0c0fd18ada390e
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-11 22:38:27 +0100

    outer join

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,5 @@
+-- select and group
+SELECT tv_genres.name as genre, count(tv_show_genres.genre_id) as number_of_shows
+WHERE tv_show_genres IS NOT NULL
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
