48240b6fabfd6f0577ab7d4478292de0ce7285f3
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-11 22:17:57 +0100

    order by

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,4 @@
 -- select
 SELECT tv_shows.title, tv_show_genres.genre_id FROM
-tv_shows LEFT JOIN  tv_show_genres ON tv_shows.id = tv_show_genres.show_id 
-AND tv_show_genres.show_id IS NOT NULL
+tv_shows  JOIN  tv_show_genres ON tv_shows.id = tv_show_genres.show_id 
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
