cb0d80b374ab6dda85b74c30427311e13d86fa40
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-11 17:51:21 +0100

    create force

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,6 @@
 -- create force table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 id INT AUTO_INCREMENT NOT NULL UNIQUE,
 state_id INT,
 name VARCHAR(256) NOT NULL
