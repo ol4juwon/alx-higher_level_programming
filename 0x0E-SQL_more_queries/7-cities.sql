f3f3c50bfb06f0ad27a6935756e2d3c1f9f83329
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-11 17:57:39 +0100

    cities

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,5 +5,5 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 id INT AUTO_INCREMENT NOT NULL UNIQUE,
 state_id INT,
 name VARCHAR(256) NOT NULL
-FOREIGN_KEY (state_id) REFERENCES states(id)
+FOREIGN KEY (state_id) REFERENCES states(id)
 );
