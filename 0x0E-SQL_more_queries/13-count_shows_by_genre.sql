7d0edaa2f75c6bbe71e181225a205c868e357701
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-11 22:42:09 +0100

    AS not nul

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- select and group
 SELECT tv_genres.name AS genre, count(tv_show_genres.genre_id) AS number_of_shows
-WHERE tv_show_genres IS NOT NULL
+WHERE tv_show_genres.genre_id IS NOT NULL
 GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
