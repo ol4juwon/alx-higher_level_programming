e94a3e70e3b976c8741fe37bac1e72c6a67f323b
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-11 22:31:37 +0100

    outer join

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,4 +1,4 @@
 -- select
 SELECT tv_shows.title, tv_show_genres.genre_id FROM
-tv_shows RIGHT JOIN  tv_show_genres ON tv_shows.id = tv_show_genres.show_id 
+tv_shows LEFT JOIN  tv_show_genres ON tv_shows.id = tv_show_genres.show_id 
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
