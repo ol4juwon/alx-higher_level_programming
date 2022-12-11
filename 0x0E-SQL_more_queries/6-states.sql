7dac1a2723717719732e4f5cc3e312831e321551
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-11 17:37:56 +0100

    create force

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- create force table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+id INT AUTO_INCREMENT NOT NULL UNIQUE,
+name VARCHAR(256) NOT NULL
+);
