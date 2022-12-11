626aac7b089a5c8ada32c6036c02a79100ca7832
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-11 15:04:33 +0100

    list desc

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,4 @@
 -- group
-SELECT score, count(score) AS number FROM second_table
-GROUP BY score, ORDER BY score DESC;
+SELECT score, COUNT(score) AS number FROM second_table
+GROUP BY score
+ORDER BY score DESC;
