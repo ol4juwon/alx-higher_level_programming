84ffbf1b8afb86080d31d4ab7a0f9007110413dd
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-11 14:30:58 +0100

    create tables

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,4 @@
 -- create first table;
-CREATE TABLE IF NOT EXISTS first_table  (
-    `id` int(11) DEFAULT NULL,
-  `name` varchar(256) DEFAULT NULL,
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 ;
+CREATE TABLE IF NOT EXISTS first_table (
+id INT,
+name VARCHAR(256));
