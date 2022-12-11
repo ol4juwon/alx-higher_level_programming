3a64e1e7f85a5232a399a0e88f84c966947b8a44
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-11 22:04:00 +0100

    asc shows

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
 -- select
 SELECT tv_shows.title, tv_show_genres.genre_id FROM
 tv_shows, tv_show_genres WHERE tv_shows.genre_id IS NOT NULL
-ORDER BY tv_shows.title ASC, ORDER BY tv_show_genres.genre_id ASC;
+ORDER BY tv_shows.title ASC AND tv_show_genres.genre_id ASC;
